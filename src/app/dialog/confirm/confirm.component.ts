ce5be8cce72855e79d2a1cc48a540234b8df35e6
Jim <James.C.KIrcher@gmail.com>
2019-07-15 17:14:07 -0400

    Add confirm dialog

---
+++ src/app/dialog/confirm/confirm.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-confirm',
+  templateUrl: './confirm.component.html',
+  styleUrls: ['./confirm.component.scss']
+})
+export class ConfirmComponent implements OnInit {
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { transformId: string, reportName: string },
+    private dialogRef: MatDialogRef<ConfirmComponent>
+  ) { }
+
+  ngOnInit() {
+  }
+
+  dismiss() {
+    this.dialogRef.close(false);
+  }
+
+  confirm() {
+    this.dialogRef.close(true);
+  }
+}
